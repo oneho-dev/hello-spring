3c90560deab4ed1a585a162baffd51ccfabcd9cf
one-dev <a624810@kakao.com>
2022-11-10 15:59:10 +0900

    통합테스트 구현 JPA 구현

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -53,8 +53,8 @@ class MemberServiceTest {
         //when
 
         memberService.join(member1);
-
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
 
 //        assertThrows(NullPointerException.class, () -> memberService.join(member2));
